84e0e3f19db76a1a719807b366e5b2abd91efd34
bmachlin <ben@machlin.net>
2020-06-03 23:31:09 -0600

    css changes, lil blug

--- nav.js
+++ nav.js
@@ -1,9 +1,9 @@
 function toggle(self) {
     self.classList.toggle('change');
-    let x = document.getElementsByTagName("nav")[0];
-    if (x.className === "") {
-        x.className = "responsive";
+    let nav = document.getElementsByTagName("nav")[0];
+    if (nav.className === "") {
+        nav.className = "responsive";
     } else {
-        x.className = "";
+        nav.className = "";
     }
 }

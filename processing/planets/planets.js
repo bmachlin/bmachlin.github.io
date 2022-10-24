46b7f8b997becec0c8e64576556d290745c352ef
Ben <ben@machlin.net>
2022-10-23 18:34:41 -0600

    adding more pcessing sketches and links

---
+++ processing/planets/planets.js
@@ -0,0 +1,227 @@
+let sketchName = "planets";
+let numPlanets;
+let stars;
+let inputElem;
+let starsAngle = 0;
+let fps = 30;
+
+
+class Moon {
+
+  constructor(planet, id) {
+    this.id = id;
+    this.infront = random(1) < 0.5;
+    this.intransit = true;
+    this.planet = planet;
+
+    this.R = random(this.planet.R / 20, this.planet.R / 5); // base radius
+    this.r = this.R;
+
+    this.min = -planet.r - this.r;
+    this.max = planet.r + this.r;
+
+    this.pos = int(random(this.min + 1, this.max));
+    this.vel = random(6/fps, 30/fps);
+
+    this.col = color(random(0, 255), random(50, 255), random(255));
+  }
+
+  render() {
+    noStroke();
+    fill(this.col);
+    ellipse(int(this.pos), 0, this.r, this.r);
+  }
+
+  update() {
+    let mult = this.infront ? 1 : -1;
+
+    if ((this.pos <= this.min || this.pos >= this.max) && this.intransit) {
+      this.intransit = false;
+      this.infront = !this.infront;
+    } else if (this.pos > this.min && this.pos < this.max) {
+      this.intransit = true;
+    }
+
+    this.pos += mult * this.vel;
+
+    // scale radius to match moon's 'distance'
+    if (this.infront)
+      this.r = this.R * map(abs(this.pos), 0, (this.max - this.min) / 2, 1.2, 1);
+    else
+      this.r = this.R * map(abs(this.pos), 0, (this.max - this.min) / 2, 0.8, 1);
+  }
+
+}
+
+class Planet {
+  constructor(id, radius, x, y, numMoons) {
+    this.init(id, radius, x, y, numMoons);
+  }
+
+  init(id, radius, x, y, numMoons) {
+    this.id = id;
+    this.numMoons = numMoons;
+    this.R = radius;
+    this.r = radius;
+    this.col = color(random(0, 255), random(50, 255), random(255));
+
+    this.x = x;
+    this.y = y;
+    this.xvel = random(-50 / fps, 50 / fps);
+    this.yvel = random(-50 / fps, 50 / fps);
+
+    this.moonAngle = random(0, TAU);
+    this.moonAngleVel = random(-TAU / fps / 10, TAU / fps / 10);
+    this.moons = [];
+    for (let i = 0; i < numMoons; i++) {
+      this.moons.push(new Moon(this, i));
+    }
+  }
+
+  render() {
+    push();
+    translate(this.x, this.y);
+    
+    // shadow
+    rotate(starsAngle);
+    noStroke();
+    fill(0, 100);
+    ellipse(this.r * 1.2, this.r * 1.2, this.r, this.r);
+    rotate(-starsAngle);
+    
+    rotate(this.moonAngle);
+
+    // moons behind
+    this.moons.forEach((m) => { if(!m.infront) m.render(); });
+
+    // planet
+    noStroke();
+    fill(this.col);
+    ellipse(0, 0, this.r, this.r);
+    
+    // moons in front
+    this.moons.forEach((m) => { if(m.infront) m.render(); });
+
+
+    pop();
+  }
+
+  update() {
+    this.x += this.xvel;
+    this.y += this.yvel;
+
+    if (random(1) < 1 / fps) {
+      this.moonAngle += this.moonAngleVel;
+    }
+
+    this.moons.forEach((m) => m.update());
+
+    if (this.outOfBounds())
+      this.reset();
+  }
+
+  reset() {
+    let x = 0;
+    let y = 0;
+    let side = int(random(4));
+    switch(int(random(4))) {
+      case 0: //top
+        x = random(width);
+        y = -this.R * 2;
+        break;
+      case 1: //right
+        x = width + this.R * 2;
+        y = random(height);
+        break;
+      case 2: //bottom
+        x = random(width);
+        y = height + this.R * 2;
+        break;
+      case 3: //left
+        x = -this.R * 2;
+        y = random(height);
+        break;
+    }
+    this.init(this.id, this.R, x, y, int(random(1, 8)));
+  }
+
+  outOfBounds() {
+    return (this.x + this.r * 2.5 < 0 ||
+      this.x - this.r * 2.5 > width ||
+      this.y + this.r * 2.5 < 0 ||
+      this.y - this.r * 2.5 > height);
+  }
+}
+
+function NewPlanet(id) {
+  return new Planet(id, random(width / 50, width / 20),
+  random(0, width), random(0, height),
+  int(random(1, 8)));
+}
+
+function setup() {
+  let cnv = createCanvas(500, 500);
+  cnv.id(sketchName + "-canvas");
+  ellipseMode(RADIUS);
+  frameRate(fps);
+  activate();
+}
+
+function activate() {
+  inputElem = document.querySelector("#planetsInput");
+  planets = [];
+  stars = [];
+  numPlanets = getSliderValue(inputElem, 10);
+
+  for (let i = 0; i < numPlanets; i++) {
+    planets.push(NewPlanet(i));
+  }
+  
+  for(let x = -width*1.5; x < width*1.5; x += width/50) {
+    for(let y = -height*1.5; y < height*1.5; y += height/50) {
+      if (random(1) < 0.1) {
+        stars.push(new p5.Vector(x+random(-10,10), y+random(-10,10)));
+        stars.push(new p5.Vector(random(1,2), random(1,2)));
+        stars.push(color(random(175,255), random(175,255), random(175,255)));
+      }
+    }
+  }
+}
+
+function reset() { activate(); }
+
+function draw() {
+  background(0);
+  
+  push();
+  rotate(starsAngle);
+  starsAngle += 1/fps/TAU/10;
+  for(let i = 0; i < stars.length-2; i+= 3) {
+    fill(stars[i+2]);
+    ellipse(stars[i].x, stars[i].y, stars[i+1].x, stars[i+1].y);
+  }
+  pop();
+
+  numPlanets = getSliderValue(inputElem, 10);
+  adjustPlanetPopulation();
+  for (let p of planets) {
+    p.render();
+    p.update();
+  }
+}
+
+function adjustPlanetPopulation() {
+  if (numPlanets == planets.length) return;
+  if (numPlanets < 0) numPlanets = 0;
+
+  if (numPlanets > planets.length) {
+    while (numPlanets > planets.length) {
+      planets.push(NewPlanet(planets.length));
+    }
+  }
+  else if (numPlanets < planets.length) {
+    while (numPlanets < planets.length) {
+      planets.splice(Math.floor(Math.random()*planets.length), 1);
+    }
+  }
+}
---
+++ processing/shape-shapes/shape-shapes.js
@@ -0,0 +1,254 @@
+let sketchName = "shape-shapes";
+let numDots;
+let dots;
+let margin = 30;
+let fps = 10;
+let date = new Date();
+let seed = date.getTime();
+let saveLoop = false;
+
+class Line {
+    constructor(m, bx, y) {
+        this.m = m;
+        this.b = y == null ? bx : y - m * bx;
+        this.xint = -this.b / m;
+        this.yint = this.b;
+    }
+
+    getY(x) {
+        return this.m * x + this.b;
+    }
+    getX(y) {
+        return (y - this.b) / this.m;
+    }
+    inX(x) {
+        return x >= 0 && x < width;
+    }
+    inY(y) {
+        return y >= 0 && y < height;
+    }
+    isect(other) {
+        let x = (other.b - this.b) / (this.m - other.m);
+        let y = this.getY(x);
+        return createVector(x, y);
+    }
+
+    render() {
+        strokeWeight(1);
+        stroke(240);
+
+        let xBeg = -this.b / this.m;           // x when y is 0
+        let xEnd = (height - this.b) / this.m; // x when y is height
+        let yBeg = this.b;                     // y when x is 0
+        let yEnd = this.m * width + this.b;    // y when x is width
+        xBeg = round(xBeg, 0);
+        xEnd = round(xEnd, 0);
+        yBeg = round(yBeg, 0);
+        yEnd = round(yEnd, 0);
+
+        let hits = this.inX(xBeg) + this.inX(xEnd) + this.inY(yBeg) + this.inY(yEnd);
+        if (hits == 2) {
+            let points = []
+            if (this.inX(xBeg)) {
+                points.push(createVector(xBeg, 0));
+            }
+            if (this.inX(xEnd)) {
+                points.push(createVector(xEnd, height));
+            }
+            if (this.inY(yBeg)) {
+                points.push(createVector(0, yBeg));
+            }
+            if (this.inY(yEnd)) {
+                points.push(createVector(width, yEnd));
+            }
+            line(points[0].x, points[0].y, points[1].x, points[1].y);
+        } else {
+            print("line outside canvas");
+            print(this);
+        }
+    }
+}
+
+class Cell {
+
+    constructor(id, x, y) {
+        this.id = id;
+        this.col = color(random(100, 255), random(100, 255), random(100, 255));
+
+        this.x = x;
+        this.y = y;
+        this.mPoints = [];
+        this.mLines = []; // lines orth to midpoint with other dots
+        for (let i = 0; i < numDots; i++) {
+            this.mLines.push(null);
+            this.mPoints.push(null);
+        }
+        this.closest = [];
+    }
+
+    calcClosest(num) {
+        // create array of dists matching dot id
+        this.closest = [];
+        let dists = []
+        for (let i = 0; i < numDots; i++) {
+            dists.push(dist(this.x, this.y, dots[i].x, dots[i].y));
+        }
+        dists[this.id] = width * height;
+        let distsCopy = [...dists];
+        // sort dists and pick ids by their distance
+        dists.sort((a, b) => a - b);
+        for (let i = 0; i < num; i++) {
+            this.closest.push(distsCopy.indexOf(dists[i]));
+        }
+        this.sortClosest()
+    }
+
+    // sort closest like a clock
+    sortClosest() {
+        if (this.closest.length <= 3)
+            return;
+        // calc center
+        let minx = width, maxx = 0, miny = height, maxy = 0;
+        for (let i = 0; i < this.closest.length; i++) {
+            let dot = dots[this.closest[i]];
+            if (dot.x < minx) minx = dot.x;
+            if (dot.x > maxx) maxx = dot.x;
+            if (dot.y < miny) miny = dot.y;
+            if (dot.y > maxy) maxy = dot.y;
+        }
+        let center = createVector((maxx + minx) / 2, (maxy + miny) / 2);
+
+        // calc angles
+        let angles = []
+        for (let i = 0; i < this.closest.length; i++) {
+            let dot = dots[this.closest[i]];
+            let a = atan2(dot.y - center.y, dot.x - center.x);
+            angles.push(a);
+        }
+        // sort angles
+        let anglesCopy = [...angles];
+        angles.sort((a, b) => a - b);
+        let tempClosest = [];
+        // use sorted angles to get orig index of angle in list
+        // use index in list to get index of dot in orig closest list. gross
+        for (let i = 0; i < angles.length; i++) {
+            tempClosest.push(this.closest[anglesCopy.indexOf(angles[i])]);
+        }
+        this.closest = tempClosest;
+    }
+
+    render() {
+        stroke(this.col);
+        fill(this.col);
+
+        // render cell shape
+        if (this.id < 10000) {
+            strokeWeight(0);
+            beginShape();
+            for (let i = 0; i < this.closest.length; i++) {
+                if (this.mPoints[this.closest[i]]) {
+                    vertex(this.mPoints[this.closest[i]].x, this.mPoints[this.closest[i]].y);
+                }
+            }
+            endShape(CLOSE);
+        }
+
+        // render cell location
+        // strokeWeight(1);
+        // ellipse(this.x, this.y, 10, 10);
+        // text(str(this.id), this.x - 5, this.y - 7);
+    }
+
+    update() {
+        this.x += map(noise(this.x, frameCount), 0, 1, -2, 2);
+        this.y += map(noise(this.y, frameCount), 0, 1, -2, 2);
+    }
+}
+
+function newCell(index) {
+    dots[index] = new Cell(index, 0, 0);
+    dots[index].x = random(margin, width - margin);
+    dots[index].y = random(margin, height - margin);
+}
+
+function setup() {
+    let cnv = createCanvas(500, 500);
+    cnv.id(sketchName + "-canvas");
+    frameRate(fps);
+
+    activate();
+}
+
+function activate() {
+    numDots = getSliderValue(document.querySelector("#shapesInput"), 100);
+    dots = [];
+
+    // create cells
+    for (var i = 0; i < numDots; i++) {
+        newCell(i);
+    }
+
+    calcMidpoints();
+    for (let i = 0; i < numDots; i++) {
+        dots[i].calcClosest(random([3, 4, 5]));
+    }
+}
+
+function reset() { activate(); }
+
+function draw() {
+    background(20);
+    for (let i = 0; i < numDots; i++) {
+        dots[i].render();
+        dots[i].update();
+    }
+    if (saveLoop) {
+        saveCanvas('midpoints' + str(seed), 'png');
+    }
+    calcMidpoints();
+}
+
+
+function closest(x, y) {
+    let minDist = width + height;
+    let minId = 0;
+    for (let i = 0; i < numDots; i++) {
+        if (dist(x, y, dots[i].x, dots[i].y) < minDist) {
+            minDist = dist(x, y, dots[i].x, dots[i].y);
+            minId = i;
+        }
+    }
+    return dots[minId];
+}
+
+function calcMidpoints() {
+    // calc and add midpoints
+    for (i = 0; i < numDots - 1; i++) {
+        for (var j = i + 1; j < numDots; j++) {
+            let xdif = dots[j].x - dots[i].x;
+            let ydif = dots[j].y - dots[i].y;
+            let midline = new Line(-xdif / ydif, dots[i].x + xdif / 2, dots[i].y + ydif / 2);
+            dots[i].mLines[j] = midline;
+            dots[j].mLines[i] = midline;
+            dots[i].mPoints[j] = createVector(dots[i].x + xdif / 2, dots[i].y + ydif / 2);
+            dots[j].mPoints[i] = createVector(dots[i].x + xdif / 2, dots[i].y + ydif / 2);
+            // draw midpoints
+            // stroke(240);
+            // strokeWeight(10);
+            // point(dots[i].x + xdif / 2, dots[i].y + ydif / 2);
+        }
+    }
+}
+
+function mousePressed() {
+    if (mouseX < 0 || mouseX > width || mouseY < 0 || mouseY > height)
+        return;
+    print("add")
+    numDots++;
+    newCell(numDots-1);
+    dots[numDots-1].x = mouseX;
+    dots[numDots-1].y = mouseY;
+    for (let i = 0; i < numDots; i++) {
+        dots[i].calcClosest(random([3, 4, 5]));
+    }
+}
